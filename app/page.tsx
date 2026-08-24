ba1bb0495673df866f57225d1ccadfcec909482c
marcoantonio1206 <marcopaglinawan225@gmail.com>
2026-08-24 20:27:10 +0800

    Linked the course bank to the website

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import courses from './data/courseBankV3.json';
+
 export default function Home() {
+  // This will print the data to your terminal
+  console.log(courses);
+
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Welcome to Ano Course Mo?
+        Data Linked!
       </h1>
     </main>
   );
